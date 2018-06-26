0d85aff98c20e0839d32938202650124521d3cc1
Yohan Maury <mauryyohan1@gmail.com>
2018-06-26 16:10:01 +0200

    B

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from '@angular/router';
 import { ConnectComponent } from './connect/connect.component';
-import { FormPersonnageComponent } from './form-personnage/form-personnage.component';
 import { PersonnageComponent } from './personnage/personnage.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 const routes: Routes = [
   {path: '', redirectTo: 'connect', pathMatch: 'full'},
   {path: 'connect', component: ConnectComponent},
-  {path: 'creationPerso', component: FormPersonnageComponent}
+  {path: 'createPerso', component: PersonnageComponent}
 ];
 
 @NgModule({
