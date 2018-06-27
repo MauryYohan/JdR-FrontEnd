b6c5bf6f4efd74f4c5df8acb4d76f2adc86a6ea6
Yohan Maury <mauryyohan1@gmail.com>
2018-06-27 15:26:43 +0200

    up

--- src/app/connect/connect.component.ts
+++ src/app/connect/connect.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UtilisateurService} from '../utilisateur.service';
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.component.html',
@@ -11,9 +12,38 @@ export class ConnectComponent implements OnInit {
   loginForm: FormGroup;
   formSubmitted = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private us : UtilisateurService) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      //Declarer nos elements de formulaire
+      'dateCreation': ['',
+        Validators.compose([
+          Validators.required])],
+      'description': ['',
+        Validators.compose([
+          Validators.required])],
+      'email': ['',
+        Validators.compose([
+          Validators.required,
+          Validators.min(10),
+          Validators.max(1000)])],
+      'isOpen': ['',
+        Validators.compose([
+          Validators.required])],
+      'level': ['',
+        Validators.compose([
+          Validators.required])],
+      'progress': ['',
+        Validators.compose([
+          Validators.required])],
+      'titre': ['',
+        Validators.compose([
+          Validators.required])],
+      'type': ['',
+        Validators.compose([
+          Validators.required])]
+    });
   }
 
 }
