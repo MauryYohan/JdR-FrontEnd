bc7105bb8846fe0b4a0ffcf141a3a4cd8af4f08c
DG <damiengombert@hotmail.fr>
2018-06-28 14:49:54 +0200

    Auto stash before merge of "master" and "origin/master"
    commit fiche perso

--- src/app/fiche-personnage/fiche-personnage.component.ts
+++ src/app/fiche-personnage/fiche-personnage.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Personnage} from '../personnage';
+import {Router} from '@angular/router';
+import {PersonnageService} from '../personnage.service';
+import {Utilisateur} from '../utilisateur';
 
 @Component({
   selector: 'app-fiche-personnage',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FichePersonnageComponent implements OnInit {
 
-  constructor() { }
+  @Input() personnage: Personnage;
+  personnageform: FormGroup ;
+  formSubmitted = false;
+  formtitle='Add';
+  display: boolean = false;
+  utilisateur : Utilisateur
+
+  constructor(private fb: FormBuilder, private router: Router, private ps: PersonnageService) { }
+  @Output() output = new EventEmitter();
 
   ngOnInit() {
+
+    this.personnageform = this.fb.group({
+      'id': [this.utilisateur.id],
+      'nom': [this.personnage.nom],
+      'classe': [this.personnage.classe],
+      'pv': [this.personnage.pv],
+      'puissance': [this.personnage.puissance, [Validators.compose([Validators.required, Validators.min(1), Validators.max(10)])]],
+      'defense': [this.personnage.defense],
+      'esprit': [this.personnage.esprit],
+      'intelligence': [this.personnage.intelligence],
+      'init': [this.personnage.init],
+      'equipement': [this.personnage.equipement],
+      'inventaire': [this.personnage.inventaire],
+      'background': [this.personnage.background],
+      'race': [this.personnage.race],
+      'sexe': [this.personnage.sexe],
+      //'id_joueur': ['']
+    });
   }
+
+  onSubmit(){
+    this.formSubmitted = true;
+    if(this.personnageform.valid) {
+      if(this.personnageform.controls['id'].value>0){
+        this.ps.update(this.personnageform.value).subscribe(
+          personnage=>{
+            this.output.emit({'sev':'success', 'sum':'Update successfull!', 'detail': 'Personnage mis à jour:'+personnage.id});
+          })
+      }
+    }
+  }
+
+  showAddDialog(){
+    this.formtitle='Add';
+    this.personnage = new Personnage();
+    this.showDialog();
+  }
+
+  showDialog() {
+    this.formSubmitted = false;
+    this.display = true;
+    console.log(this.personnage);
+  }
+
 
 }
